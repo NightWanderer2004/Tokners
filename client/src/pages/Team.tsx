400cada3891e63a2ff1511fee0955680f315d241
Eduard <goby145csgo@gmail.com>
2022-05-04 14:59:26 +0200

    team page

--- client/src/pages/Team.tsx
+++ client/src/pages/Team.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import OurTeam from '../components/teamPage/OurTeam'
 
 const Team: React.FC = () => {
-   return <div>Team</div>
+   return (
+      <main className='flex-1'>
+         <OurTeam />
+      </main>
+   )
 }
 
 export default Team
