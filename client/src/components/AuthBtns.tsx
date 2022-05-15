4d9427ed00714c72f9cb854cfe9d8c02000bc71d
Eduard <goby145csgo@gmail.com>
2022-05-15 13:06:37 +0200

    check if auth

--- client/src/components/AuthBtns.tsx
+++ client/src/components/AuthBtns.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useLocation, Link } from 'react-router-dom'
+import { logoutUser } from '../actions/authActions'
 import s from '../styles/navbar.module.scss'
 
 interface Props {
@@ -8,9 +10,28 @@ interface Props {
 
 const AuthBtns: React.FC<Props> = ({ clickHandler }) => {
    const currentUrl = useLocation()
+   const dispatch = useDispatch()
+   const { isAuthenticated, user } = useSelector((state: any) => state.auth)
 
-   return (
-      <ul className={s.authBtns}>
+   const onLogoutClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault()
+      dispatch(logoutUser() as any)
+   }
+
+   const userLinks = (
+      <>
+         <li onClick={clickHandler}>
+            <Link to='/profile'>
+               <button>{user.login}</button>
+            </Link>
+         </li>
+         <li onClick={clickHandler}>
+            <button onClick={onLogoutClick}>Logout</button>
+         </li>
+      </>
+   )
+   const guestLinks = (
+      <>
          {currentUrl.pathname !== '/login' && (
             <li onClick={clickHandler}>
                <Link to='/login'>
@@ -25,8 +46,10 @@ const AuthBtns: React.FC<Props> = ({ clickHandler }) => {
                </Link>
             </li>
          )}
-      </ul>
+      </>
    )
+
+   return <ul className={s.authBtns}>{isAuthenticated ? userLinks : guestLinks}</ul>
 }
 
 export default AuthBtns
