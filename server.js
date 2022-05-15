64ae3b37057a3347cb93e8bcca83db3cdcc4d424
Eduard <goby145csgo@gmail.com>
2022-05-15 15:13:47 +0200

    new middleware

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
+
 app.use(express.static(path.resolve(__dirname, './client/build')))
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(passport.initialize())
 require('./config/passport')(passport)
