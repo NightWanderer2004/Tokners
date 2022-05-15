6f2a37683cb2ca52d0defe7680a25a1432ee7278
Eduard <goby145csgo@gmail.com>
2022-05-15 15:37:00 +0200

    fix middleware

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const port = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
-app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') app.use(express.static('/client/build'))
 
 app.use(passport.initialize())
 require('./config/passport')(passport)
