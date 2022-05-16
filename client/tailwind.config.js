065760108ce47fa626b4acbafd383086526a8c38
Eduard <goby145csgo@gmail.com>
2022-05-16 17:31:06 +0200

    add colors in config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
    content: ['./src/**/*.{js,jsx,ts,tsx}'],
    theme: {
-      backgroundSize: {
-         full: '100%',
+      extend: {
+         colors: {
+            bgc: '#21263b',
+            primary: '#0F1320',
+            secondary: '#171B29',
+            Green: '#22a75d',
+            Blue: '#2278d4',
+            Yellow: '#ffd100',
+            Red: '#e20613',
+         },
+         backgroundSize: {
+            full: '100%',
+         },
       },
-      extend: {},
    },
    plugins: [],
 }
