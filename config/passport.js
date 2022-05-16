1c0d4424ad24de2804bb4ea295eef9902e44c283
Eduard <goby145csgo@gmail.com>
2022-05-16 09:42:19 +0200

    rename const

--- config/passport.js
+++ config/passport.js
@@ -3,14 +3,14 @@ const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const User = require('../models/User')
 
-const opts = {
+const options = {
    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
    secretOrKey: process.env.ACCESS_TOKEN_KEY,
 }
 
 module.exports = passport => {
    passport.use(
-      new JwtStrategy(opts, (jwt_payload, done) => {
+      new JwtStrategy(options, (jwt_payload, done) => {
          User.findById(jwt_payload.id)
             .then(user => {
                if (user) return done(null, user)
