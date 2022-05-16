754c8e1f72c3ac615d769bb444d00daf31eb508c
Eduard <goby145csgo@gmail.com>
2022-05-16 23:06:03 +0200

    improve to path.resolve()

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.json())
 app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, '/client/build')))
+   app.use(express.static(path.resolve(__dirname, './client/build')))
    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
    })
 }
 
